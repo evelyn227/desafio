1fa5dd9f1d2c780cadd15114374ded5d9087ac56
evelyn227 <135766624+evelyn227@users.noreply.github.com>
2024-08-06 11:35:10 -0300

    Add files via upload

---
+++ desafio12.js
@@ -0,0 +1,14 @@
+var jogahtml = window.document.getElementById('saida')
+
+function clique1() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a primeira opção = botão 1</p>'
+}
+
+function clique2() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a segunda opção = botão 2</p>'
+}
+
+function clique3() {
+    jogahtml.innerHTML += '<p>Você clicou e escolheu a terceira opção = botão 3</p>'
+}
+
